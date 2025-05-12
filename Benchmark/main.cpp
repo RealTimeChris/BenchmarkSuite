7b9365913914889045d4dad7e9ee240eaae71f96
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2025-05-11 21:03:35 -0400

    Testing with cache-clearing set to disable by default.

--- Benchmark/main.cpp
+++ Benchmark/main.cpp
@@ -1,45 +1,61 @@
-#include <iostream>
-#include <vector>
-#include <array>
-#include <bit>
+#include <BnchSwt/BenchmarkSuite.hpp>
 
-inline static constexpr float fp32_from_bits(uint32_t w) {
-	return std::bit_cast<float>(w);
-}
+struct test_non_type_01 {
+	uint64_t test_array{};
+};
 
-inline static constexpr uint32_t fp32_to_bits(float f) {
-	return std::bit_cast<uint32_t>(f);
-}
+struct test_non_type_02 {
+	uint64_t test_array{};
+};
 
-inline static constexpr float oiml_compute_fp16_to_fp32(uint16_t h) {
-	const uint32_t w	 = static_cast<uint32_t>(h) << 16;
-	const uint32_t sign	 = w & 0x80000000u;
-	const uint32_t two_w = w + w;
+struct test_non_type_03 {
+	uint64_t test_array{};
+};
 
-	constexpr uint32_t exp_offset = 0xE0u << 23;
-	constexpr float exp_scale	  = fp32_from_bits(0x7800000u);
-	const float normalized_value  = fp32_from_bits((two_w >> 4) + exp_offset) * exp_scale;
-
-	constexpr uint32_t magic_mask  = 126u << 23;
-	constexpr float magic_bias	   = 0.5f;
-	const float denormalized_value = fp32_from_bits((two_w >> 17) | magic_mask) - magic_bias;
-
-	constexpr uint32_t denormalized_cutoff = 1u << 27;
-	const uint32_t result				   = sign | (two_w < denormalized_cutoff ? fp32_to_bits(denormalized_value) : fp32_to_bits(normalized_value));
-	return fp32_from_bits(result);
-}
-
-alignas(64)
-inline const std::array<float, (1 << 16)> fp16_to_fp32_table{ [] {
-	std::array<float, (1 << 16)> returnValues{};
-	for (uint32_t x = 0; x < (1 << 16); ++x) {
-		returnValues[x] = oiml_compute_fp16_to_fp32(static_cast<uint16_t>(x));
+template<uint64_t arg> BNCH_SWT_INLINE static constexpr double fp32_from_bits_fn(uint64_t w) {
+	uint64_t test_val_new01{ static_cast<uint64_t>(rand()) };
+	uint64_t val_new{};
+	for (size_t x = 0; x < sizeof(arg); ++x) {
+		val_new += arg + rand();
 	}
-	return returnValues;
-}() };
+	return std::bit_cast<double>(val_new + w);
+}
+
+template<uint64_t arg> struct fp32_from_bits {
+	BNCH_SWT_INLINE static constexpr double impl(uint32_t w) {
+		uint64_t test_val_new01{ static_cast<uint64_t>(rand()) };
+		uint64_t val_new{};
+		for (size_t x = 0; x < sizeof(arg); ++x) {
+			val_new += arg + rand();
+		}
+		return std::bit_cast<double>(val_new + w);
+	}
+};
+
+template<uint64_t arg_new> struct fp32_from_bits_static {
+	static constexpr auto arg{ arg_new };
+	BNCH_SWT_INLINE static constexpr double impl(uint32_t w) {
+		uint64_t val_new{};
+		for (size_t x = 0; x < sizeof(arg); ++x) {
+			val_new += arg + rand();
+		}
+		return std::bit_cast<double>(val_new + w);
+	}
+};
 
 int main() {
-	auto new_value = fp16_to_fp32_table[0];
-	std::cout << "CURRENT VALUE: " << new_value << std::endl;
+	srand(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+	uint64_t test_val_new01{ static_cast<uint64_t>(rand()) };
+	uint64_t test_val_new02{ static_cast<uint64_t>(rand()) };
+	uint64_t test_val_new03{ static_cast<uint64_t>(rand()) };
+	static constexpr uint64_t test_val_01{ 23225343 };
+	static constexpr uint64_t test_val_02{ 234533223 };
+	static constexpr uint64_t test_val_03{ 2323223 };
+	auto value01 = fp32_from_bits_fn<test_val_01>(test_val_new01);
+	auto value02 = fp32_from_bits<test_val_02>::impl(test_val_new02);
+	auto value03 = fp32_from_bits_static<test_val_03>::impl(test_val_new03);
+	std::cout << "VAL01: " << value01 << std::endl;
+	std::cout << "VAL02: " << value02 << std::endl;
+	std::cout << "VAL03: " << value03 << std::endl;
 	return 0;
 }
