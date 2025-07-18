4734abb69925335c4eb25d25b58610a039c38998
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2025-07-18 17:17:42 -0400

    Testing with cache-clearing set to disable by default.

--- Benchmark/main.cpp
+++ Benchmark/main.cpp
@@ -1,45 +1,89 @@
+#include <BnchSwt/BenchmarkSuite.hpp>
+#include <thread>
 #include <iostream>
 #include <vector>
+#include <mutex>
 #include <array>
+#include <queue>
+#include <latch>
 #include <bit>
 
-inline static constexpr float fp32_from_bits(uint32_t w) {
-	return std::bit_cast<float>(w);
-}
-
-inline static constexpr uint32_t fp32_to_bits(float f) {
-	return std::bit_cast<uint32_t>(f);
-}
-
-inline static constexpr float oiml_compute_fp16_to_fp32(uint16_t h) {
-	const uint32_t w	 = static_cast<uint32_t>(h) << 16;
-	const uint32_t sign	 = w & 0x80000000u;
-	const uint32_t two_w = w + w;
-
-	constexpr uint32_t exp_offset = 0xE0u << 23;
-	constexpr float exp_scale	  = fp32_from_bits(0x7800000u);
-	const float normalized_value  = fp32_from_bits((two_w >> 4) + exp_offset) * exp_scale;
-
-	constexpr uint32_t magic_mask  = 126u << 23;
-	constexpr float magic_bias	   = 0.5f;
-	const float denormalized_value = fp32_from_bits((two_w >> 17) | magic_mask) - magic_bias;
-
-	constexpr uint32_t denormalized_cutoff = 1u << 27;
-	const uint32_t result				   = sign | (two_w < denormalized_cutoff ? fp32_to_bits(denormalized_value) : fp32_to_bits(normalized_value));
-	return fp32_from_bits(result);
-}
-
-alignas(64)
-inline const std::array<float, (1 << 16)> fp16_to_fp32_table{ [] {
-	std::array<float, (1 << 16)> returnValues{};
-	for (uint32_t x = 0; x < (1 << 16); ++x) {
-		returnValues[x] = oiml_compute_fp16_to_fp32(static_cast<uint16_t>(x));
+BNCH_SWT_INLINE void mul_mat_scalar_f32(const float* A, const float* B, float* C, size_t M, size_t K, size_t N) {
+	const float* a_ptr = A;
+	const float* b_ptr = B;
+	float* c_ptr	   = C;
+	for (size_t i = 0; i < M; i++) {
+		for (size_t j = 0; j < N; j++) {
+			float sum = 0;
+			for (size_t k = 0; k < K; k++) {
+				size_t a_idx = i * K + k;
+				size_t b_idx = k * N + j;
+				sum += a_ptr[a_idx] * b_ptr[b_idx];
+			}
+			size_t c_idx = i * N + j;
+			c_ptr[c_idx] = sum;
+		}
 	}
-	return returnValues;
-}() };
+}
+
+BNCH_SWT_INLINE void mul_mat_linear_f32(const float* A, const float* B, float* C, size_t M, size_t K, size_t N) {
+	const float* a_ptr = A;
+	float* c_ptr	   = C;
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j++) {
+			const float* b_ptr = B + j;
+			float sum		   = 0;
+			for (int k = 0; k < K; k++) {
+				sum += (*a_ptr++) * (*b_ptr);
+				b_ptr += N;
+			}
+			*c_ptr++ = sum;
+			a_ptr -= K;
+		}
+		a_ptr += K;
+	}
+}
 
 int main() {
-	auto new_value = fp16_to_fp32_table[0];
-	std::cout << "CURRENT VALUE: " << new_value << std::endl;
+	static constexpr uint64_t test_iteration_count{ 20 };
+	static constexpr size_t dim_a{ 512 };
+	static constexpr size_t dim_b{ 512 };
+	static constexpr size_t dim_c{ 512 };
+
+	std::vector<float> input01{};
+	input01.resize(dim_a * dim_b);
+	for (size_t x = 0; x < dim_a * dim_b;++x) {
+		input01[x] = bnch_swt::random_generator::generateValue<float>();
+	}
+	std::vector<float> input02{};
+	input02.resize(dim_a * dim_b);
+	for (size_t x = 0; x < dim_a * dim_b; ++x) {
+		input02[x] = bnch_swt::random_generator::generateValue<float>();
+	}
+	std::vector<float> output{};
+	output.resize(dim_a * dim_b);
+
+	struct standard_indexing {
+		BNCH_SWT_INLINE static size_t impl(const float* A, const float* B, float* C, size_t M, size_t K, size_t N) {
+			mul_mat_scalar_f32(A, B, C, M, K, N);
+			bnch_swt::doNotOptimizeAway(C);
+			return M * K * sizeof(float);
+		};
+	};
+
+	struct linear_indexing {
+		BNCH_SWT_INLINE static size_t impl(const float* A, const float* B, float* C, size_t M, size_t K, size_t N) {
+			mul_mat_linear_f32(A, B, C, M, K, N);
+			bnch_swt::doNotOptimizeAway(C);
+			return M * K * sizeof(float);
+		};
+	};
+
+	bnch_swt::benchmark_stage<"test_stage", test_iteration_count, test_iteration_count / 10>::runBenchmark<"standard_indexing", standard_indexing>(input01.data(), input02.data(),
+		output.data(), dim_a, dim_b, dim_c);
+	bnch_swt::benchmark_stage<"test_stage", test_iteration_count, test_iteration_count / 10>::runBenchmark<"linear_indexing", linear_indexing>(input01.data(), input02.data(),
+		output.data(), dim_a, dim_b, dim_c);
+
+	bnch_swt::benchmark_stage<"test_stage", test_iteration_count, test_iteration_count / 10>::printResults();
 	return 0;
 }
